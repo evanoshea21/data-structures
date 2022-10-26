b6bf2689067e6fbc8e16be8b496c0056994cc453
Evan OShea <evanoshea21@gmail.com>
2022-10-26 15:05:44 -0700

    Functional Shared Complete (both Stack & Queue

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,40 @@
 var Queue = function() {
+  var instance = {};
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  instance.enqueue = queueMethods.enqueue;
+  instance.dequeue = queueMethods.dequeue;
+  instance.size = queueMethods.size;
+
+  return instance;
 };
 
 var queueMethods = {};
+
+var getKeyz = function(thisArg) {
+  return Object.keys(thisArg).filter(key => !['enqueue','dequeue','size'].includes(key));
+};
+
+queueMethods.enqueue = function(val) {
+ if(getKeyz(this).length < 1) {
+   this[0] = val;
+ } else {
+   for(var i = getKeyz(this).length; i > 0; i--) {
+    this[i] = this[i-1];
+   }
+   this[0] = val;
+ }
+};
+
+queueMethods.dequeue = function() {
+  var index = getKeyz(this).length;
+  var deqVal = this[index-1];
+  delete this[index-1];
+  return deqVal;
+};
+
+queueMethods.size = function() {
+  return getKeyz(this).length;
+};
 
 
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,33 @@
 var Stack = function() {
+  var instance = {};
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  instance.push = stackMethods.push;
+  instance.pop = stackMethods.pop;
+  instance.size = stackMethods.size;
+
+  return instance;
 };
 
 var stackMethods = {};
+
+var getKeys = function(thisArg) {
+  return Object.keys(thisArg).filter(key => !['push','pop','size'].includes(key));
+};
+
+var currentIndex = 0;
+stackMethods.push = function(val) {
+  this[currentIndex] = val;
+  currentIndex++;
+};
+stackMethods.pop = function() {
+  currentIndex--;
+ var poppedVal = this[currentIndex];
+ delete this[currentIndex];
+ return poppedVal;
+};
+stackMethods.size = function() {
+  return getKeys(this).length;
+};
 
 
