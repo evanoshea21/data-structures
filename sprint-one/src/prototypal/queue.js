c9e0ac7f0eb9b0df6c39dfeebf9868e9e88a9aa0
Evan OShea <evanoshea21@gmail.com>
2022-10-26 16:29:31 -0700

    Prototypal Pattern Complete

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,37 @@
 var Queue = function() {
+  var instance = Object.create(queueMethods);
+  instance.storage = {};
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  return instance;
 };
 
 var queueMethods = {};
+
+queueMethods.enqueue = function(val) {
+  if(this.size() < 1) {
+    this[0] = val;
+    this.storage[0] = val;
+  } else {
+    for(var i = this.size(); i > 0; i--) {
+      this[i] = this[i-1];
+      this.storage[i] = this.storage[i-1];
+    }
+    this[0] = val;
+    this.storage[0] = val;
+  }
+};
+
+queueMethods.dequeue = function() {
+  var deqVal = this[this.size() - 1];
+  console.log('deqReturn: ', deqVal);
+  delete this[this.size() - 1];
+  delete this.storage[this.size() - 1];
+  return deqVal;
+};
+
+queueMethods.size = function() {
+  return Object.keys(this.storage).length;
+};
 
 
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,29 @@
 var Stack = function() {
+ var instance = Object.create(stackMethods);
+
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  return instance;
 };
 
 var stackMethods = {};
+var currentIndex = 0;
+
+stackMethods.push = function(val) {
+  this[currentIndex] = val;
+  currentIndex++;
+};
+
+stackMethods.pop = function() {
+  currentIndex--;
+  var poppedVal = this[currentIndex];
+  delete this[currentIndex];
+  return poppedVal;
+};
+
+stackMethods.size = function() {
+  return Object.keys(this).filter(key => !['push','pop','size'].includes(key)).length;
+};
+
 
 
