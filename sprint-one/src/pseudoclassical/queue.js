8f774b448e4b3e960966aab53849416041faa694
Evan OShea <evanoshea21@gmail.com>
2022-10-26 16:43:08 -0700

    Pseudoclassical pattern Complete

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,32 @@
 var Queue = function() {
+  this.storage = {};
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+};
+
+Queue.prototype.enqueue = function(val) {
+  if(this.size() < 1) {
+    this[0] = val;
+    this.storage[0] = val;
+  } else {
+    for(var i = this.size(); i > 0; i--) {
+      this[i] = this[i-1];
+      this.storage[i] = this.storage[i-1];
+    }
+    this[0] = val;
+    this.storage[0] = val;
+  }
+};
+
+Queue.prototype.dequeue = function() {
+  var deqVal = this[this.size() - 1];
+  delete this[this.size() - 1];
+  delete this.storage[this.size() - 1];
+  return deqVal;
+};
+
+Queue.prototype.size = function() {
+  return Object.keys(this.storage).length;
 };
 
 
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,26 @@
 var Stack = function() {
+  this.storage = {};
+  this.index = 0;
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+};
+
+Stack.prototype.push = function(val) {
+    this[this.index] = val;
+    this.storage[this.index] = val;
+    this.index++;
+};
+
+Stack.prototype.pop = function() {
+  this.index--;
+  var poppedVal = this[this.index];
+  delete this[this.index];
+  delete this.storage[this.index];
+  return poppedVal;
+};
+
+Stack.prototype.size = function() {
+  return Object.keys(this.storage).length;
 };
 
 
