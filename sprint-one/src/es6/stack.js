8319089a53b0dc55bc8cdcea4812cb7095a5ceef
Evan OShea <evanoshea21@gmail.com>
2022-10-26 17:14:11 -0700

    ES6 Class Pattern Complete

--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -1,7 +1,25 @@
 class Stack {
+  constructor() {
+    this.storage = {};
+
+  }
+
+  push(val) {
+    this[this.size()] = val;
+    this.storage[this.size()] = val;
+  }
+  pop() {
+    var poppedVal = this[this.size()-1];
+    delete this[this.size()-1];
+    delete this.storage[this.size()-1];
+    return poppedVal;
+  }
+
+  size() {
+  return Object.keys(this.storage).length;
+  }
+
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
-  constructor() {
-  }
 
 }
