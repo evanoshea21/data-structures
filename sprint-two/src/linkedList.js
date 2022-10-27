14a400c4156383dbb733a9a1b851c8addf36bdb9
Evan OShea <evanoshea21@gmail.com>
2022-10-27 12:14:47 -0700

    LinkedListComplete

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,15 +1,42 @@
+// I - value
+// O - none //modifying the tree
+// E - list is empty // just place it in
+
 var LinkedList = function() {
   var list = {};
   list.head = null;
   list.tail = null;
 
   list.addToTail = function(value) {
+    //debugger;
+    var node = Node(value);
+
+    if(this.tail === null) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      this.tail.next = node;
+      this.tail = node;
+    }
   };
 
   list.removeHead = function() {
+    var OGheadNode = list.head.value;
+    this.head = list.head.next;
+    return OGheadNode;
   };
 
-  list.contains = function(target) {
+  list.contains = function(target, currentNode) {
+    var isFound = false;
+    var currentNode = currentNode || list.head;
+    if(currentNode.value === target) {
+      return true;
+    } else if(currentNode.next === null) {
+      return false;
+    } else {
+      isFound =  list.contains(target, currentNode.next);
+    }
+    return isFound;
   };
 
   return list;
